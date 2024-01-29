e82946c324027e472ad076ebc009ef1ee071b597
Ayush <ayushayush.sadwal@gmail.com>
2024-01-29 16:47:41 +0530

    version 0.1

---
+++ tf-aks/aks.tf
@@ -0,0 +1,37 @@
+/*
+resource "azurerm_resource_group" "example" {
+  name     = "django-app"
+  location = "West Europe"
+} */
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "poll-app-aks1"
+  location            = "West Europe"
+  resource_group_name = "dfghjhj"
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
